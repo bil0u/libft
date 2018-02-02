13734922c4dea9429284bfa2d943c1803696a4d6
upopee <upopee@student.42.fr>
2018-02-02 16:37:05 +0100

    Corrected paths

--- libft.h
+++ libft.h
@@ -6,20 +6,20 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 11:44:25 by upopee            #+#    #+#             */
-/*   Updated: 2018/02/02 16:09:34 by upopee           ###   ########.fr       */
+/*   Updated: 2018/02/02 16:36:41 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include "characters.h"
-# include "ft_printf.h"
-# include "linked_lists.h"
-# include "maths.h"
-# include "memory.h"
-# include "read_write.h"
-# include "strings.h"
-# include "wide_strings.h"
+# include "./characters/includes/characters.h"
+# include "./ft_printf/includes/ft_printf.h"
+# include "./linked_lists/includes/linked_lists.h"
+# include "./maths/includes/maths.h"
+# include "./memory/includes/memory.h"
+# include "./read_write/includes/read_write.h"
+# include "./strings/includes/strings.h"
+# include "./wide_strings/includes/wide_strings.h"
 
 #endif
