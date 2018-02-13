b81fb6584837fb84633e5f790bd00faf1763c024
Ugo Popée <ugo.popee@me.com>
2018-02-13 02:00:04 +0100

    Added protection

--- linked_lists/sources/ft_lstdelone.c
+++ linked_lists/sources/ft_lstdelone.c
@@ -6,12 +6,13 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 11:48:38 by upopee            #+#    #+#             */
-/*   Updated: 2018/02/02 15:43:08 by upopee           ###   ########.fr       */
+/*   Updated: 2018/02/12 16:41:50 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "linked_lists.h"
+#include "memory.h"
 
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 {
@@ -21,7 +22,7 @@ void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 		{
 			if (del)
 				(*del)((*alst)->content, (*alst)->content_size);
-			free(*alst);
+			ft_memdel((void **)&(*alst));
 		}
 		*alst = NULL;
 	}
