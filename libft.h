74cca872c8560e0b5012fc6a3cd7a7635c9c3954
upopee <upopee@student.42.fr>
2018-03-29 08:22:46 +0200

    SUCCESS and FAILURE macros added

--- libft.h
+++ libft.h
@@ -6,25 +6,27 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 11:44:25 by upopee            #+#    #+#             */
-/*   Updated: 2018/03/09 07:20:08 by upopee           ###   ########.fr       */
+/*   Updated: 2018/03/29 00:49:08 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# define TRUE 1
-# define FALSE 0
+# define TRUE		1
+# define FALSE		0
+# define SUCCESS	0
+# define FAILURE	-1
 
-# define BSET(x, bit) ((x) |= (bit))
-# define BUNSET(x, bit) ((x) &= ~(bit))
-# define BCOPY(x, y, bit) ((y) = ((y) & ~(bit)) | ((x) & (bit)))
-# define BIS_SET(x, bit) (((x) & (bit)) != 0)
+# define BSET(x, bit)			((x) |= (bit))
+# define BUNSET(x, bit)			((x) &= ~(bit))
+# define BCOPY(x, y, bit)		((y) = ((y) & ~(bit)) | ((x) & (bit)))
+# define BIS_SET(x, bit)		(((x) & (bit)) != 0)
 
-# define SWAP_UINT16(x) (((x) >> 8) | ((x) << 8))
-# define SWAP_UINT32LEFT(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8))
-# define SWAP_UINT32RIGHT(x) ((((x) & 0x0000FF00) << 8) | ((x) << 24))
-# define SWAP_UINT32(x) (SWAP_UINT32LEFT(x) | SWAP_UINT32RIGHT(x))
+# define SWAP_UINT16(x)			(((x) >> 8) | ((x) << 8))
+# define SWAP_UINT32LEFT(x)		(((x) >> 24) | (((x) & 0x00FF0000) >> 8))
+# define SWAP_UINT32RIGHT(x)	((((x) & 0x0000FF00) << 8) | ((x) << 24))
+# define SWAP_UINT32(x)			(SWAP_UINT32LEFT(x) | SWAP_UINT32RIGHT(x))
 
 # include "./characters/includes/characters.h"
 # include "./ft_printf/includes/ft_printf.h"
