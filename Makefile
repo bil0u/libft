0063b016e9e4af2c6566c8c0ed811bc41e7a7394
Ugo Popée <ugo.popee@me.com>
2018-04-05 19:35:22 +0200

    Added colors for printf

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: upopee <upopee@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/28 11:42:57 by upopee            #+#    #+#              #
-#    Updated: 2018/03/20 15:56:47 by upopee           ###   ########.fr        #
+#    Updated: 2018/04/05 18:36:25 by upopee           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@
 
 NAME =				libft.a
 CC =				gcc
-CFLAGS =			-Wall -Werror -Wextra
+CFLAGS =			-Wall -Werror -Wextra -g
 CPPFLAGS =			-I $(MEMORY_DIR)/$(INC_DIR) \
 					-I $(CHAR_DIR)/$(INC_DIR) \
 					-I $(STR_DIR)/$(INC_DIR) \
--- ft_printf/includes/colors_table.h
+++ ft_printf/includes/colors_table.h
@@ -6,7 +6,7 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 15:51:29 by upopee            #+#    #+#             */
-/*   Updated: 2018/02/02 15:10:03 by upopee           ###   ########.fr       */
+/*   Updated: 2018/04/05 17:42:44 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define COLORS_TABLE_H
 
 # define C_EOC "\e[0m"
+
 # define CF_DEFAULT "\e[39m"
 # define CF_BLACK "\e[30m"
 # define CF_WHITE "\e[97m"
@@ -24,7 +25,17 @@
 # define CF_MAGENTA "\e[35m"
 # define CF_CYAN "\e[36m"
 
-# define NB_COLORS 10
+# define CB_DEFAULT "\e[49m"
+# define CB_BLACK "\e[40m"
+# define CB_WHITE "\e[47m"
+# define CB_RED "\e[41m"
+# define CB_GREEN "\e[42m"
+# define CB_YELLOW "\e[43m"
+# define CB_BLUE "\e[44m"
+# define CB_MAGENTA "\e[45m"
+# define CB_CYAN "\e[46m"
+
+# define NB_COLORS 20
 # define COLOR_MAX_LEN 10
 
 char	g_colors_table[NB_COLORS][COLOR_MAX_LEN] =
@@ -38,7 +49,16 @@ char	g_colors_table[NB_COLORS][COLOR_MAX_LEN] =
 	CF_YELLOW,
 	CF_BLUE,
 	CF_MAGENTA,
-	CF_CYAN
+	CF_CYAN,
+	CB_DEFAULT,
+	CB_BLACK,
+	CB_WHITE,
+	CB_RED,
+	CB_GREEN,
+	CB_YELLOW,
+	CB_BLUE,
+	CB_MAGENTA,
+	CB_CYAN
 };
 
 char	g_colors_ref_table[NB_COLORS][COLOR_MAX_LEN] =
@@ -52,7 +72,16 @@ char	g_colors_ref_table[NB_COLORS][COLOR_MAX_LEN] =
 	"YELLOW}",
 	"BLUE}",
 	"MAGENTA}",
-	"CYAN}"
+	"CYAN}",
+	"B_DEFAULT}",
+	"B_BLACK}",
+	"B_WHITE}",
+	"B_RED}",
+	"B_GREEN}",
+	"B_YELLOW}",
+	"B_BLUE}",
+	"B_MAGENTA}",
+	"B_CYAN}"
 };
 
 #endif
