c98b794567da2f0ccb6f3ddff1cd5cd3dc53d41f
upopee <upopee@student.42.fr>
2018-05-03 23:11:49 +0200

    norm update

--- memory/includes/memory.h
+++ memory/includes/memory.h
@@ -6,7 +6,7 @@
 /*   By: upopee <upopee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 19:33:07 by upopee            #+#    #+#             */
-/*   Updated: 2018/05/02 04:15:00 by upopee           ###   ########.fr       */
+/*   Updated: 2018/05/03 23:11:34 by upopee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void	ft_bzero(void *s, size_t n);
 
 void	*ft_u16set(void *b, int c, size_t len);
 void	*ft_u32set(void *b, int c, size_t len);
-
 
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
